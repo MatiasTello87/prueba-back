0a669b2d600a4dc8331172dd5f333fb2cf05e94c
Matias Tello <matiasfernandotello@gmail.com>
2023-08-02 16:15:32 -0300

    primer commit

---
+++ index.js
@@ -0,0 +1,15 @@
+/*
+Dar primera instrucción a nuestra compu para que quede escuchando peticiones en un determinado puerto 1- solicitar un módule http
+*/
+const http = require("http")
+
+//función controladora 
+function requestController (){
+    console.log("recibimos una nueva requestaa")
+}
+
+// creamos servidor con esa librería
+// configurar el server
+const server = http.createServer(requestController)
+
+server.listen(4000)
